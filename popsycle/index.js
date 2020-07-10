e0a5de2a98f16904594101ca77ae760a1e6f5a9d
Lyons <Gavin.Lyons@avl.com>
2020-07-10 14:24:36 +0200

    Add Support for Active Windows

--- popsycle/index.js
+++ popsycle/index.js
@@ -1,5 +1,5 @@
 const TrayWindow = require("../electron-tray-window/src");
-
+const activeWin = require('active-win');
 //https://www.npmjs.com/package/keyboard-layout
 //const KeyboardLayout = require('keyboard-layout')
 //let subscription = null
@@ -11,8 +11,30 @@ const path = require("path");
 app.whenReady().then(() => {
   // Register a 'CommandOrControl+X' shortcut listener.
   const ret = globalShortcut.register('CommandOrControl+Shift+K', () => {
-    console.log('CommandOrControl+Shift+K is pressed')
-
+    console.log('CommandOrControl+Shift+K is pressed');
+    (async () => {
+      console.log(await activeWin());
+      /*
+      {
+          title: 'Unicorns - Google Search',
+          id: 5762,
+          bounds: {
+              x: 0,
+              y: 0,
+              height: 900,
+              width: 1440
+          },
+          owner: {
+              name: 'Google Chrome',
+              processId: 310,
+              bundleId: 'com.google.Chrome',
+              path: '/Applications/Google Chrome.app'
+          },
+          url: 'https://sindresorhus.com/unicorn',
+          memoryUsage: 11015432
+      }
+      */
+    })();
     TrayWindow.toggleWindow()
   })
 
@@ -65,12 +87,15 @@ ipcMain.on("tray-window-clicked", (e, a) => {
   console.log("clicked the tray icon");
   //console.log(e.window)
   //console.log(e.tray)
+
+  
 });
 
 ipcMain.on("tray-window-visible", (e, a) => {
   console.log("tray window is visible now");
   //console.log(e.window)
   //console.log(e.tray)
+
 });
 
 ipcMain.on("tray-window-hidden", (e, a) => {
@@ -78,3 +103,4 @@ ipcMain.on("tray-window-hidden", (e, a) => {
   //console.log(e.window)
   //console.log(e.tray)
 });
+
